d109eb18d21391fcb682972dd1860c58d7a60fc0
Youri <youri.yvdl@gmail.com>
2020-06-18 09:38:10 +0200

    FormationService Tests

--- LogicsTests/Services/FormationServiceTests.cs
+++ LogicsTests/Services/FormationServiceTests.cs
@@ -18,7 +18,7 @@ namespace Logics.Services.Tests
     {
         private FormationService _formationService;
 
-        public async Task CreateDatabase()
+        public async Task CreateService()
         {
             var options = new DbContextOptionsBuilder<LineupDbContext>()
                 .UseInMemoryDatabase(databaseName: "LineupTestDb")
@@ -100,7 +100,7 @@ namespace Logics.Services.Tests
         public async Task GetAllFormationsTest()
         {
             //Arrange
-            await CreateDatabase();
+            await CreateService();
             int teamId = 1;
             List<Formation> formations = new List<Formation>();
 
@@ -115,7 +115,7 @@ namespace Logics.Services.Tests
         public async Task AddFormationTest()
         {
             //Arrange
-            await CreateDatabase();
+            await CreateService();
             Formation formation = new Formation()
             {
                 Name = "Test-4",
@@ -138,7 +138,7 @@ namespace Logics.Services.Tests
         public async Task AddPlayerPositionTest()
         {
             //Arrange
-            await CreateDatabase();
+            await CreateService();
             PlayerPosition playerPosition = new PlayerPosition()
             {
                 FormationId = 4,
@@ -163,7 +163,7 @@ namespace Logics.Services.Tests
         public async Task DeleteFormationTest()
         {
             //Arrange
-            await CreateDatabase();
+            await CreateService();
             int teamId = 1;
             int id = 4;
 
@@ -173,6 +173,187 @@ namespace Logics.Services.Tests
             //Assert
             List<Formation> formations = await _formationService.GetAllFormations(teamId);
             Assert.AreEqual(3, formations.Count);
+        }
+
+        [TestMethod()]
+        public async Task EditFormationTest()
+        {
+            //Arrange
+            await CreateService();
+            Formation formation = new Formation()
+            {
+                Id = 1,
+                Name = "Changed",
+                TeamId = 1
+            };
+
+            //Act
+            await _formationService.EditFormation(formation);
+
+            //Assert
+            Formation f = await _formationService.GetFormation(formation.Id);
+            Assert.AreEqual(formation.Id, f.Id);
+            Assert.AreEqual(formation.Name, f.Name);
+        }
+
+        [TestMethod()]
+        public async Task EditPlayerPositionTest()
+        {
+            //Arrange
+            await CreateService();
+            PlayerPosition playerPosition = new PlayerPosition()
+            {
+                Id = 1,
+                FormationId = 1,
+                PlayerId = 1,
+                Position = Position.Leftwing
+            };
+
+            //Act
+            await _formationService.EditPlayerPosition(playerPosition);
+
+            //Assert
+            PlayerPosition pp = await _formationService.GetPlayerPosition(playerPosition.Id);
+            Assert.AreEqual(playerPosition.Id, pp.Id);
+            Assert.AreEqual(playerPosition.PlayerId, pp.PlayerId);
+            Assert.AreEqual(playerPosition.Position, pp.Position);
+        }
+
+        [TestMethod()]
+        public async Task GetAllPlayerPositionsTest()
+        {
+            //Arrange
+            await CreateService();
+            int formationId = 2;
+            List<PlayerPosition> expectedPlayerPositions = new List<PlayerPosition>();
+            for (int i = 1; i < 11; i++)
+            {
+                PlayerPosition pp = new PlayerPosition()
+                {
+                    FormationId = formationId,
+                    PlayerId = i,
+                    Id = i + 11,
+                    Position = (Position)i
+                };
+                expectedPlayerPositions.Add(pp);
+            }
+
+            //Act
+            List<PlayerPosition> playerPositions = await _formationService.GetAllPlayerPositions(formationId);
+
+            //Assert
+            Assert.AreEqual(expectedPlayerPositions.Count, playerPositions.Count);
+            Assert.AreEqual(expectedPlayerPositions[0].Id, playerPositions[0].Id);
+            Assert.AreEqual(expectedPlayerPositions[4].Id, playerPositions[4].Id);
+        }
+
+        [TestMethod()]
+        public async Task GetPlayerPositionTest()
+        {
+            //Arrange
+            await CreateService();
+            int playerPositionId = 6;
+
+            //Act
+            PlayerPosition playerPosition = await _formationService.GetPlayerPosition(playerPositionId);
+
+            //Assert
+            Assert.AreEqual(6, playerPosition.Id);
+            Assert.AreEqual(6, playerPosition.PlayerId);
+            Assert.AreEqual(1, playerPosition.FormationId);
+        }
+
+        [TestMethod()]
+        public async Task GetFormationTest()
+        {
+            //Arrange
+            await CreateService();
+            int formationId = 2;
+
+            //Act
+            Formation formation = await _formationService.GetFormation(formationId);
+
+            //Assert
+            Assert.AreEqual("Test-2", formation.Name);
+            Assert.AreEqual(formationId, formation.Id);
+        }
+
+        [TestMethod()]
+        public async Task CheckFormationTest()
+        {
+            //Arrange
+            await CreateService();
+            List<Position> positions = new List<Position>();
+            for (int i = 0; i < 11; i++)
+            {
+                positions.Add(Position.Substitute);
+            }
+
+            //Act
+            List<string> errors = _formationService.CheckFormation(positions);
+
+            //Assert
+            Assert.AreEqual(2, errors.Count);
+            Assert.AreEqual("You need exactly 11 field players", errors[0]);
+            Assert.AreEqual("You need exactly 1 goalkeeper", errors[1]);
+        }
+
+        [TestMethod()]
+        public async Task CheckFormationTest1()
+        {
+            //Arrange
+            await CreateService();
+            List<Position> positions = new List<Position>();
+            for (int i = 0; i < 10; i++)
+            {
+                positions.Add(Position.Substitute);
+            }
+            positions.Add(Position.Goalkeeper);
+
+            //Act
+            List<string> errors = _formationService.CheckFormation(positions);
+
+            //Assert
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual("You need exactly 11 field players", errors[0]);
+        }
+
+        [TestMethod()]
+        public async Task CheckFormationTest2()
+        {
+            //Arrange
+            await CreateService();
+            List<Position> positions = new List<Position>();
+            for (int i = 0; i < 11; i++)
+            {
+                positions.Add(Position.Striker);
+            }
+
+            //Act
+            List<string> errors = _formationService.CheckFormation(positions);
+
+            //Assert
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual("You need exactly 1 goalkeeper", errors[0]);
+        }
+
+        [TestMethod()]
+        public async Task CheckFormationTest3()
+        {
+            //Arrange
+            await CreateService();
+            List<Position> positions = new List<Position>();
+            for (int i = 0; i < 10; i++)
+            {
+                positions.Add(Position.Striker);
+            }
+            positions.Add(Position.Goalkeeper);
+
+            //Act
+            List<string> errors = _formationService.CheckFormation(positions);
+
+            //Assert
+            Assert.AreEqual(0, errors.Count);
         }
     }
 }
